781faa566f0b51a9607ad90824a56ebbef51a1d5
stigus96 <stighelgebruset@msn.com>
2020-08-30 13:11:04 +0200

    First commit

---
+++ src/rubikkannonsesystem/REST.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rubikkannonsesystem;
+
+/**
+ *
+ * @author Stigus
+ */
+public class REST {
+    
+    
+    
+    
+    public List<Item> getItems(){
+        
+    }
+    
+    
+    
+    
+    public Response purchase(Long itemid) {
+        
+    }
+    
+    public Response delete(Long itemid) {
+        
+    }
+    
+    public Response addItem(String title, String description, BigDecimal price, FormDataMultiPart Photos) {
+        
+    }
+    
+    public Response getPhoto(String name, int width) {
+    }
+    
+    
+    public Response changePassword(String userid, String password) {
+       
+    }
+    
+}
